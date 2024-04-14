86624d62b2c550d69db91f892aa02e15ff490043
Abdulnur930 <hussenabdulnur@gmail.com>
2024-04-14 20:53:40 +0300

    reset

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,14 +2,12 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
-import { Rowlist } from "../../components/Row/RowList/Rowlist";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Banner />
-      <Rowlist/>
+      <Banner/>
       <Footer />
     </>
   );
